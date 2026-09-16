f0798f1a2e03318500cd355d7fa774aefdf4ea20
sujiswetha17 <sujiswetha17@gmail.com>
2026-09-16 21:00:50 +0530

    Connect frontend to FastAPI

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,27 @@
-function analyzeJob() {
+async function analyzeJob() {
     const jobTitle = document.getElementById("job_title").value;
     const company = document.getElementById("company").value;
     const location = document.getElementById("location").value;
     const salary = document.getElementById("salary").value;
     const description = document.getElementById("description").value;
 
-    console.log("Job Title:", jobTitle);
-    console.log("Company:", company);
-    console.log("Location:", location);
-    console.log("Salary:", salary);
-    console.log("Description:", description);
+    const jobData = {
+        job_title: jobTitle,
+        company: company,
+        location: location,
+        salary: Number(salary),
+        description: description
+    };
+
+    const response = await fetch("http://127.0.0.1:8000/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jobData)
+    });
+
+    const result = await response.json();
+    document.getElementById("result").innerText = result.result;
+
 }
