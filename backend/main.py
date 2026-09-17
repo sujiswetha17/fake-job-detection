00e2786be503772034662d76c337ff656807d91b
sujiswetha17 <sujiswetha17@gmail.com>
2026-09-17 20:55:36 +0530

    Clean repository and update backend

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from .database import jobs_collection
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .schemas import JobPosting
@@ -15,16 +16,20 @@ app.add_middleware(
 @app.get("/")
 def home():
     return {"message": "Fake Job Detection API is running"}
-
-
 @app.post("/predict")
 def predict(job: JobPosting):
+    job_data = job.model_dump()
+
+    jobs_collection.insert_one(job_data)
+
     return {
         "job_title": job.job_title,
         "company": job.company,
         "description": job.description,
         "location": job.location,
         "salary": job.salary,
-        "result": "Prediction endpoint is ready"
+        "result": "Job saved to MongoDB"
     }
+
+
     
